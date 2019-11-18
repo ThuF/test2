39f2adcdd143784c3a021f320bf5304cb1852290
ThuF <jordan.p.pavlov@gmail.com>
2019-11-18 14:54:26 +0000

    Initial Commit

---
+++ dancho/helloWorld.js
@@ -0,0 +1,21 @@
+var database = require('db/v3/database');
+var response = require('http/v3/response');
+
+var connection = database.getConnection();
+try {
+    var statement = connection.prepareStatement("select * from DIRIGIBLE_EXTENSIONS");
+    var resultSet = statement.executeQuery();
+    while (resultSet.next()) {
+        response.println("[path]: " + resultSet.getString("EXTENSION_LOCATION"));
+    }
+    resultSet.close();
+    statement.close();
+} catch(e) {
+    console.trace(e);
+    response.println(e.message);
+} finally {
+    connection.close();
+}
+
+response.flush();
+response.close();
