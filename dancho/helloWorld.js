1f19c9fd8f8853f18769db75f460fad3b0dd874e
ThuF <jordan.p.pavlov@gmail.com>
2020-04-21 18:25:31 +0300

    Updates HelloWorld.js 2

--- dancho/helloWorld.js
+++ dancho/helloWorld.js
@@ -1,21 +1,6 @@
-var database = require('db/v4/database');
-var response = require('http/v4/response');
+var response = require("http/v4/response");
 
-var connection = database.getConnection();
-try {
-    var statement = connection.prepareStatement("select * from DIRIGIBLE_EXTENSIONS");
-    var resultSet = statement.executeQuery();
-    while (resultSet.next()) {
-        response.println("[path] = " + resultSet.getString("EXTENSION_LOCATION"));
-    }
-    resultSet.close();
-    statement.close();
-} catch(e) {
-    console.trace(e);
-    response.println(e.message);
-} finally {
-    connection.close();
-}
+response.println("Hello World @");
 
 response.flush();
 response.close();
